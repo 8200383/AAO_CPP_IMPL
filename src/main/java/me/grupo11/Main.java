c57b83e7fbb0f0629bcbb4849ce1f95a0e5b8d57
Micael Dias <8200383@estg.ipp.pt>
2022-06-18 20:33:59 +0100

    Add SemiEulerian Graph

--- src/main/java/me/grupo11/Main.java
+++ src/main/java/me/grupo11/Main.java
@@ -8,11 +8,12 @@ public class Main {
     public static void main(String[] args) throws IOException, ParseException {
 
         GraphADT graph = new GraphBuilder()
-                .importJson("eulerian.json")
+                .importJson("semi-eulerian.json")
                 .buildUndirectedGraph();
 
         System.out.println(graph.isConnected());
         System.out.println(graph.plot(false));
+        System.out.println(graph.getSingleNodes().toString());
         System.out.println(graph);
     }
 }
